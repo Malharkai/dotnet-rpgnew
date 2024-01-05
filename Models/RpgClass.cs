6f9dbcdf02c993d7ed150335467039321dad5c94
Malharkai <shotdragon96@hotmail.com>
2024-01-05 15:00:29 +0100

    #

--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -1,13 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace dotnet_rpg.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RpgClass
     {
-        Fighter,
-        Cleric,
-        Druid,
-        Ranger,
-        Mage,
-        Rouge
+        Fighter = 1,
+        Cleric = 2,
+        Druid = 3,
+        Ranger = 4,
+        Mage = 5,
+        Rouge = 6
 
     }
 }
